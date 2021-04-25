5f740f06fe2c76485b5b56f3c075477f2eb9e43c
Sergey Revenko <s.amoralez84@gmail.com>
2021-04-25 12:41:45 +0300

    modified lection's files

---
+++ src/test/java/ru/netology/manager/MovieManagerTestNonEmpty.java
@@ -0,0 +1,10 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Movie;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MovieManagerTestNonEmpty {
+
+}
