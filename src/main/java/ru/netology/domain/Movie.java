f8ae44206889366129371ab2222cf8cc3e424455
Sergey Revenko <s.amoralez84@gmail.com>
2021-05-16 22:40:53 +0300

    add logic in MovieManager

--- src/main/java/ru/netology/domain/Movie.java
+++ src/main/java/ru/netology/domain/Movie.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 public class Movie {
-  private int id;
-  private String movieName;
-  private String genre;
-  }
+    private int id;
+    private String movieName;
+    private String genre;
+}
--- src/main/java/ru/netology/manager/MovieManager.java
+++ src/main/java/ru/netology/manager/MovieManager.java
@@ -1,41 +1,41 @@
 package ru.netology.manager;
 
+import net.sf.saxon.expr.Component;
 import ru.netology.domain.Movie;
 
 public class MovieManager {
-  private Movie[] items = new Movie[0];
+    private Movie[] items = new Movie[0];
+    private int number = 10;
+    private int userNumber;
 
-  public void add(Movie item) {
-    // создаём новый массив размером на единицу больше
-    int length = items.length + 1;
-    Movie[] tmp = new Movie[length];
-    // itar + tab
-    // копируем поэлементно
-    // for (int i = 0; i < items.length; i++) {
-    //   tmp[i] = items[i];
-    // }
-    System.arraycopy(items, 0, tmp, 0, items.length);
-    // кладём последним наш элемент
-    int lastIndex = tmp.length - 1;
-    tmp[lastIndex] = item;
-    items = tmp;
-  }
-
-  public Object getMovie() {
-    Movie[] result = new Movie[items.length];
-    // перебираем массив в прямом порядке
-    // но кладём в результаты в обратном
-    for (int i = 0; i < result.length; i++) {
-      int index = items.length - i - 1;
-      result[i] = items[index];
-      if (index>10){
-        return result[10];
-      }
-      if (index<=10){
-        return result;
-      }
+    public MovieManager() {
     }
-    return result;
-  }
+
+    public MovieManager(int userNumber) {
+        this.userNumber = userNumber;
+    }
+
+    public void add(Movie item) {
+        int length = items.length + 1;
+        Movie[] tmp = new Movie[length];
+        System.arraycopy(items, 0, tmp, 0, items.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = item;
+        items = tmp;
+    }
+
+    public Object getMovie() {
+        if (userNumber > number) {
+            Movie[] result = new Movie[10];
+        } else {
+            Movie[] result = new Movie[userNumber];
+        }
+        Movie[] result = new Movie[items.length];
+        for (int i = 0; i < result.length; i++) {
+            int index = items.length - i - 1;
+            result[i] = items[index];
+        }
+        return result;
+    }
 
 }
